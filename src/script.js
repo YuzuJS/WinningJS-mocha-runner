39eefe2edcb3e6920353f06849979eb4db97bfcd
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-27 17:52:02 -0500

    Initial commit with test runner app in src/.

---
+++ src/script.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    "use strict";
+
+    WinJS.Application.start();
+    mocha.setup({ ui: "bdd" });
+    connect();
+
+    function onDataReceived(data) {
+        eval(data);
+        mocha.run();
+    }
+
+    function reset() {
+        document.getElementById("mocha").innerHTML = "";
+        mocha.suite.tests = [];
+        mocha.suite.suites = [];
+    }
+
+    function connect() {
+        var connection = new WebSocket("ws://localhost:8080");
+
+        connection.onerror = function (error) {
+            console.error(error);
+            retry();
+        };
+
+        connection.onclose = function () {
+            console.log("WebSocket connection closed");
+            retry();
+        };
+
+        connection.onmessage = function (ev) {
+            onDataReceived(ev.data);
+        };
+    }
+
+    var isRetrying = false;
+    function retry() {
+        if (isRetrying) {
+            return;
+        }
+        isRetrying = true;
+
+        setTimeout(function () {
+            reset();
+            connect();
+            isRetrying = false;
+        }, 500);
+    }
+}());
