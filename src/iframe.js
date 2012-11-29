bf3781e0111f2552b8701b7971741794a81a24dc
Domenic Denicola <domenic@domenicdenicola.com>
2012-11-29 16:54:46 -0500

    Factor out iframe.js.

---
+++ src/iframe.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    "use strict";
+
+    // Mocha detects us as IE8 and refuses to give us a nice `nextTick` shim.
+    process.nextTick = setImmediate.bind(window);
+
+    // Since Mocha handles uncaught errors (using `window.onerror`), don't let them crash the app.
+    WinJS.Application.addEventListener("error", function () {
+        return true;
+    });
+
+    process.on = function (event, listener) {
+        if (event === "uncaughtException") {
+            listener._adapted = function (ev) {
+                listener(ev.detail);
+                return true;
+            };
+            WinJS.Application.addEventListener("error", listener._adapted);
+        }
+    };
+    process.removeListener = function (event, listener) {
+        if (event === "uncaughtException") {
+            WinJS.Application.removeEventListener("error", listener._adapted);
+        }
+    };
+}());
